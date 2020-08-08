fa2d76400e401b7fe0557ed7c4565e3a251c05db
ykmikan <ykmikan821@gmail.com>
2020-08-09 00:00:45 +0900

    add my setting.

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -16,38 +16,41 @@ enum custom_keycodes {
   RGB_SLD
 };
 
+#define G_C(kc) kc | 0x0800 | 0x0100 // `GUI + Ctrl + kc`
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
+ * GCS: Command + Ctrl + Space
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   -    |   1  |   2  |   3  |   4  |   5  |  (   |           |  =   |   6  |   7  |   8  |   9  |   0  |   +    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Ctr+Del|   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]  |   Y  |   U  |   I  |   O  |   P  |   DEL   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * | LALT   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| RALT   |
+ * |--------+------+------+------+------+------|  \   |           |  /   |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |Grv/L1|  '"  |AltShf|GUI/| |   `  |                                       |  '   |   _  |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        | App  | ESC  |       | Del  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                 |      |      | Down |       | Up   |        |      |
+ *                                 | Space|,/Ctrl|------|       |------| ./GUI  |Enter |
+ *                                 |      |      | LEFT |       | RIGHT|        |      |
+ *                                 `--------------------'       `-----------¥-----------'
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
-  KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),             TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
-  KC_BSPC,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
-  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),                  MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSFT,
-  LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
-                                                           ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
-                                                                          KC_HOME,                 KC_PGUP,
-                                                         KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_TAB, KC_ENT
+  KC_MINS,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LPRN,             KC_EQL,       KC_6,    KC_7,    KC_8,    KC_9,  KC_0,           KC_PLUS,
+  LGUI(KC_BSPC),     KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,     KC_LBRC,             KC_RBRC,     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,      KC_BSPC,
+  KC_LALT,          KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN),  KC_RALT,
+  KC_LSFT,          CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    KC_BSLS,              KC_SLSH, KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSFT,
+  LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), GUI_T(KC_PIPE), KC_GRV,                                            KC_QUOT,   KC_UNDS, KC_LBRC, KC_RBRC, TT(SYMB),
+                                                            ALT_T(KC_APP), KC_ESC,              KC_DEL, CTL_T(KC_ESC),
+                                                                          KC_DOWN,              KC_UP,
+                                                  KC_SPC, CTL_T(KC_COMM), KC_LEFT,              KC_RGHT, GUI_T(KC_DOT), KC_ENT
 ),
 /* Keymap 1: Symbol Layer
  *
